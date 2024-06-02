6543b8630ff3749b7c31c3bd4f6ebb00354c9190
mosesgitonga <infosec947@gmail.com>
2024-06-02 19:50:14 +0300

    sci-fi landing page created

---
+++ frontend/src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import WindowFrame from './components/window/WindowFrame';
+import './App.css'
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <WindowFrame />
+  </React.StrictMode>,
+)
