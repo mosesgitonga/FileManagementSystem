bae50a345b45476d4ea2f350234c652afdd5b827
mosesgitonga <infosec947@gmail.com>
2024-06-04 18:13:40 +0300

    circle button at theedge

---
+++ frontend/src/components/documents/documents.jsx
@@ -0,0 +1,7 @@
+// In this file,
+// we:
+//      1. List down all documents from the api endpoint
+//      2. In each div/li tag of the listed docs of we will have a send to another dept, delete doc,
+//          and view doc.
+
+
